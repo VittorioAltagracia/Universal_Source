41b8274b4ffe479bb084d11e617bb9999efb6620
VittorioAltagracia <92496399+VittorioAltagracia@users.noreply.github.com>
2023-07-21 21:38:12 -0500

    removed unnecessary image and styling for it, updated styling for ErrorToast to match the styling of the toast in the Footer component

--- src/utils/Supabase.js
+++ src/utils/Supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://teqtlancpsgidvygohhh.supabase.cox";
+const supabaseUrl = "https://teqtlancpsgidvygohhh.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRlcXRsYW5jcHNnaWR2eWdvaGhoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODE5NDc1MzgsImV4cCI6MTk5NzUyMzUzOH0.v5OWspbLsQk6SU3ptfRlgP3DH9ofCORGfxO-2dBL8lk";
 const supabase = createClient(supabaseUrl, supabaseKey);
