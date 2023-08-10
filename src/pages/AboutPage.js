44d93819bee985ad63e09a2d2ff979d3a9894e43
VittorioAltagracia <92496399+VittorioAltagracia@users.noreply.github.com>
2023-08-09 23:57:29 -0500

    update About page to render dynamic transaltion, fixed typos, translation issue and grammar

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,53 +1,24 @@
 import { Container, Row, Col } from "reactstrap";
+import { useTranslation } from "react-i18next";
+import { AboutCreator } from "../utils/translations/hardCodedUITranslations";
 const AboutPage = () => {
+  const { i18n } = useTranslation();
+
   return (
     <div>
       <Container fluid className="mt-5 mx-4">
         <Row className="align-items-start">
           <Col md="5" xs="11" className="app-info p-3">
-            <h2>About creator of this app</h2>
+            <h2>{AboutCreator.mainHeader[i18n.language]}</h2>
           </Col>
         </Row>
         <Row>
           <Col md="5" xs="11" className="app-info pb-2">
-            <p>
-              Hi, my name is <strong>Viktor</strong> or as many people call me -{" "}
-              <strong>Vittorio</strong>, and I am the developer of this
-              application. I am developing this application by utilizing almost
-              5 years of experience in tech industry as a Software Quality
-              Assurance Engineer, passion about Technology <strong>&&</strong>{" "}
-              Coding. I describe myself as someone who is extremely ambitious
-              with has an utterly high craving for continuos self-improvement
-              and learning new skills. My passion for learning and speaking
-              different languages, as well as embracing other cultures has
-              greatly influenced my life and even the idea to build this
-              application.
-            </p>
-            <h4>How was the idea born?</h4>
-            <p>
-              The idea to create this application was born when I started
-              attending a coding bootcamp in the beginning of 2023 with a goal
-              to improve my existig skills and obtain skills, that I yet had to
-              learn, to become a Software Developer. At that time, I was trying
-              to come up with some unique, original portfolio project idea.
-              Something that would solve real-world problem(s). So, given my
-              immigration background, I came up with an idea to help{" "}
-              <strong>fellow Ukranians</strong> and other nations. This idea was
-              heavily influenced by the fact that a lot of Ukrainians started
-              coming to the US thanks to U4U (uniting for Ukraine) program,
-              fleeing from the russian invasion to Ukraine that began in early
-              2022.
-              <br></br>
-              Then the initial idea of creating a software solution, that would
-              answer all the essential quesitons in a simple to understand way,
-              was backed up by my years-long experience living in this country
-              and analyzing most common struggles people face.
-            </p>
-            <p>
-              Ironically, it was almost a year that I moved to Austin, and it
-              just happened to be that I made quite a few acquintances among
-              Ukrainian people - here in Texas.
-            </p>
+            <p>{AboutCreator.$1stPart[i18n.language]}</p>
+            <h4>{AboutCreator.subHeader[i18n.language]}</h4>
+            <p>{AboutCreator.$2ndPart[i18n.language]}</p>
+            <p>{AboutCreator.$3rdPart[i18n.language]}</p>
+            <p>{AboutCreator.$4thPart[i18n.language]}</p>
           </Col>
         </Row>
       </Container>
--- src/utils/translations/hardCodedUITranslations.js
+++ src/utils/translations/hardCodedUITranslations.js
@@ -24,7 +24,7 @@ export const TextForNavbar = {
 export const AccordionAndCardText = {
   docs: {
     en: "Accepted/Required Documents",
-    uk: "Допустимі/Необхідні Документи",
+    uk: "Прийнятні/Необхідні Документи",
   },
   category: {
     en: "Category",
@@ -76,4 +76,31 @@ export const TranslatedCategories = {
     "Імміграція",
     "Фінанси",
   ],
+};
+
+export const AboutCreator = {
+  mainHeader: {
+    en: "About the creator of this app",
+    uk: "Про автора цього додатку",
+  },
+  $1stPart: {
+    en: "Hi, my name is Viktor, or as many people call me - Vittorio, and I am the developer of this application. I am developing this application by utilizing almost 5 years of experience in the tech industry as a Software Quality Assurance Engineer, with a passion for Technology && Coding. I describe myself as someone who is extremely ambitious with has an utterly high craving for continuous self-improvement and learning new skills. My passion for learning and speaking different languages, as well as embracing other cultures has greatly influenced my life and even the idea to build this application.",
+    uk: "Привіт, мене звати Віктор, але багато хто називає мене - Вітторіо, і я розробник цього додатку. Я розробляю цей додаток, використовуючи майже 5-річний досвід роботи в технологічній галузі в якості інженера із забезпечення якості програмного забезпечення (SQA), з пристрастю до технологій та програмування. Я характеризую себе як супер амбіціозну людину, з надзвичайно високим прагненням до постійного самовдосконалення та вивчення нових навичок. Моя пристрасть до вивчення та спілкування різними мовами, а також до інших культур значно вплинула на моє життя і навіть на ідею створення цього додатку.",
+  },
+  subHeader: {
+    en: "How was the idea born?",
+    uk: "Як зв`явилась ця ідея?",
+  },
+  $2ndPart: {
+    en: "The idea to create this application was born when I started attending a coding boot camp in the beginning of 2023 with a goal to improve my existing skills and obtain skills, that I yet had to learn, to become a Software Developer. At that time, I was trying to come up with some unique, original portfolio project ideas. Something that would solve a real-world problem(s). So, given my immigration background, I came up with an idea to help fellow Ukrainians and other nations. This idea was heavily influenced by the fact that a lot of Ukrainians started coming to the US thanks to U4U (uniting for Ukraine) program, fleeing from the russian invasion to Ukraine that began in early 2022.",
+    uk: "Ідея створити цей додаток народилася, коли я почав відвідувати буткемп з кодування на початку 2023 року з метою покращити свої існуючі навички та отримати навички, яких мені ще потрібно було навчитися, щоб стати розробником програмного забезпечення. У той час я намагався придумати якусь унікальну, оригінальну ідею для портфоліо-проекту. Щось, що могло б вирішити реальну проблему або декілька проблем. Тож, враховуючи мій імміграційний бекграунд, у мене виникла ідея допомогти українцям та іншим народам. На цю ідею значною мірою вплинув той факт, що багато українців почали приїжджати до США завдяки програмі U4U (Uniting for Ukraine), рятуючись від російського вторгнення в Україну, яке розпочалося на початку 2022 року.",
+  },
+  $3rdPart: {
+    en: "Then the initial idea of creating a software solution, that would answer all the essential questions in a simple-to-understand way, was backed up by my years-long experience living in this country and analyzing the most common struggles people face.",
+    uk: "І потім найперша ідея створити програмне рішення, яке б відповідало на всі основні питання в простій і зрозумілій формі, була підкріплена моїм багаторічним досвідом життя в цій країні та аналізом найпоширеніших проблем, з якими стикаються люди.",
+  },
+  $4thPart: {
+    en: "Ironically, it was almost a year since I moved to Austin, and it just happened to be that I made quite a few acquaintances among Ukrainian people - here in Texas.",
+    uk: "По іронії долі, минув майже рік з моменту, як я переїхав до Остіна, і так вийшло, що в мене з'явилося багато знайомих серед українців - тут, в Техасі.",
+  },
 };
