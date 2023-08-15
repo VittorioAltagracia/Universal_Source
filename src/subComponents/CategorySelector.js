de6e53c82d83e426a6af7aa4a29704bcbaf6ed48
VittorioAltagracia <92496399+VittorioAltagracia@users.noreply.github.com>
2023-08-15 16:48:44 -0500

    refactored category selector to prevent it from breakign app in production

--- src/subComponents/CategorySelector.js
+++ src/subComponents/CategorySelector.js
@@ -4,6 +4,7 @@ import {
   DropdownItem,
   DropdownToggle,
   DropdownMenu,
+  Nav,
 } from "reactstrap";
 import { Category } from "../utils/category";
 import { NavLink } from "react-router-dom";
@@ -24,6 +25,15 @@ const CategorySelector = () => {
   };
 
   const { i18n } = useTranslation();
+  const TranslatedCategoriesArray = Object.entries(TranslatedCategories).map(
+    ([language, categories]) => ({
+      language,
+      categories: categories.map((category, index) => ({
+        id: index,
+        name: category,
+      })),
+    })
+  );
 
   return (
     <>
@@ -34,7 +44,9 @@ const CategorySelector = () => {
           {CategorySelectorText.catSelector[i18n.language]}
         </DropdownToggle>
         <DropdownMenu>
-          {Category.map((cat) => {
+          {TranslatedCategoriesArray.find(
+            (entry) => entry.language === i18n.language
+          ).categories.map((cat) => {
             return (
               <DropdownItem key={cat.id}>
                 <NavLink
@@ -43,7 +55,6 @@ const CategorySelector = () => {
                   className="dropdown-item"
                 >
                   {cat.name}
-                  {/* {TranslatedCategories[i18n.language]} */}
                 </NavLink>
               </DropdownItem>
             );
