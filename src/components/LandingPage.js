a463877c909ddbe8fdf258df3c320c6cbba51d63
VittorioAltagracia <92496399+VittorioAltagracia@users.noreply.github.com>
2023-08-19 15:46:50 -0500

    improved responsivness to all viewports on landing page and about page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -11,12 +11,12 @@ const LandingPage = () => {
   return (
     <Container fluid className="mt-5">
       <Row className="align-items-start">
-        <Col md="4" className="app-info-2 py-3">
+        <Col md="6" lg="6" xl="4" className="app-info-2 py-3">
           <h3 style={{ color: "#000" }}>{AboutTheApp.header[i18n.language]}</h3>
         </Col>
       </Row>
       <Row className="pb-5">
-        <Col md="4" className="app-info-1">
+        <Col md="6" lg="6" xl="4" className="app-info-1">
           <Container>
             <Row className="my-3">
               <Col md="auto">
@@ -27,26 +27,26 @@ const LandingPage = () => {
         </Col>
       </Row>
       <Row className="justify-content-end mt-5">
-        <Col md="4" className="app-info-2 py-3">
+        <Col md="6" lg="6" xl="4" className="app-info-2 py-3">
           <h3 style={{ color: "#000" }}>
             {SimplicityOfUse.header[i18n.language]}
           </h3>
         </Col>
       </Row>
       <Row className="justify-content-end ">
-        <Col md="4" className="app-info-1 py-3 mb-5">
+        <Col md="6" lg="6" xl="4" className="app-info-1 py-3 mb-5">
           <p>{SimplicityOfUse.$1stPart[i18n.language]}</p>
         </Col>
       </Row>
       <Row className="justify-content-start mt-5">
-        <Col md="4" className="app-info-2 py-3">
+        <Col md="6" lg="6" xl="4" className="app-info-2 py-3">
           <h3 style={{ color: "#000" }}>
             {MultiLingualSupport.header[i18n.language]}
           </h3>
         </Col>
       </Row>
       <Row className="justify-content-start ">
-        <Col md="4" className="app-info-1 py-3">
+        <Col md="6" lg="6" xl="4" className="app-info-1 py-3">
           <p>{MultiLingualSupport.$1stPart[i18n.language]}</p>
         </Col>
       </Row>
