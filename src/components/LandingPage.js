393aae7fb8edf0b7008144e9740d150be7491a3d
VittorioAltagracia <92496399+VittorioAltagracia@users.noreply.github.com>
2023-09-10 22:52:12 -0500

    achieved desired layout for the landing page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -11,46 +11,48 @@ const LandingPage = () => {
 
   return (
     <>
-      <div className="imgRight">
-        <Container fluid className="mt-5">
-          <Row className="align-items-center justify-content-start">
-            <Col md="6" lg="6" xl="8" className="app-info-2">
-              <h3>{AboutTheApp.header[i18n.language]}</h3>
-            </Col>
-          </Row>
-          <Row className="pb-5 justify-content-start">
-            <Col md="6" lg="6" xl="8" className="app-info-1 p-0">
-              <Container>
-                <Row className="my-3">
-                  <Col md="auto">
-                    <p>{AboutTheApp.$1stPart[i18n.language]}</p>
-                  </Col>
-                </Row>
-              </Container>
-            </Col>
-          </Row>
-          <Row className="justify-content-start mt-5">
-            <Col md="6" lg="6" xl="4" className="app-info-2">
-              <h3>{SimplicityOfUse.header[i18n.language]}</h3>
-            </Col>
-          </Row>
-          <Row className="justify-content-start">
-            <Col md="6" lg="6" xl="4" className="app-info-1  mb-5">
-              <p>{SimplicityOfUse.$1stPart[i18n.language]}</p>
-            </Col>
-          </Row>
-          <Row className="justify-content-start mt-5">
-            <Col md="6" lg="6" xl="4" className="app-info-2">
-              <h3>{MultiLingualSupport.header[i18n.language]}</h3>
-            </Col>
-          </Row>
-          <Row className="justify-content-start ">
-            <Col md="6" lg="6" xl="4" className="app-info-1">
-              <p>{MultiLingualSupport.$1stPart[i18n.language]}</p>
-            </Col>
-          </Row>
-        </Container>
-      </div>
+      <Container fluid className="mt-5">
+        <Row>
+          {/* Column for text */}
+          <Col md="8" lg="8" xl="8">
+            <Row className="align-items-center justify-content-start">
+              <Col className="app-info-2">
+                <h3>{AboutTheApp.header[i18n.language]}</h3>
+              </Col>
+            </Row>
+            <Row className="pb-5 justify-content-start">
+              <Col className="app-info-1 p-0">
+                <p>{AboutTheApp.$1stPart[i18n.language]}</p>
+              </Col>
+            </Row>
+            <Row className="justify-content-start mt-5">
+              <Col className="app-info-2">
+                <h3>{SimplicityOfUse.header[i18n.language]}</h3>
+              </Col>
+            </Row>
+
+            <Row className="justify-content-start">
+              <Col className="app-info-1  mb-5">
+                <p>{SimplicityOfUse.$1stPart[i18n.language]}</p>
+              </Col>
+            </Row>
+            <Row className="justify-content-start mt-5">
+              <Col className="app-info-2">
+                <h3>{MultiLingualSupport.header[i18n.language]}</h3>
+              </Col>
+            </Row>
+            <Row className="justify-content-start ">
+              <Col className="app-info-1">
+                <p>{MultiLingualSupport.$1stPart[i18n.language]}</p>
+              </Col>
+            </Row>
+          </Col>
+          {/* Column for image */}
+          <Col md="4" lg="4" xl="4">
+            <div className="imgRight"></div>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 };
