343492d056c5ebe0fea688556b5dcf4514bb25e3
VittorioAltagracia <92496399+VittorioAltagracia@users.noreply.github.com>
2023-12-22 23:42:38 -0600

    cleaned up dependancies, removed unused dependancies, added unit tests and code coverage Instanbul, etc

--- src/utils/category.js
+++ src/utils/category.js
@@ -26,26 +26,3 @@ export const Category = [
     name: "Finances",
   },
 ];
-
-// export const Categories = {
-//   en: [
-//     { id: 0, name: "Education" },
-//     { id: 1, name: "Documents" },
-//     { id: 2, name: "Driving" },
-//     { id: 3, name: "Jobs" },
-//     { id: 4, name: "Language" },
-//     { id: 5, name: "Housing" },
-//     { id: 6, name: "Immigration" },
-//     { id: 7, name: "Finances" },
-//   ],
-//   uk: [
-//     { id: 0, name: "Освіта" },
-//     { id: 1, name: "Документи" },
-//     { id: 2, name: "Водійські права" },
-//     { id: 3, name: "Робота" },
-//     { id: 4, name: "Мова" },
-//     { id: 5, name: "Житло" },
-//     { id: 6, name: "Імміграція" },
-//     { id: 7, name: "Фінанси" },
-//   ],
-// };
